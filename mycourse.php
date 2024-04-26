1fef36ecf60c9fc6b74b02abe8ab641e070a268b
quocthanh03 <quocthanh.280603@gmail.com>
2024-04-26 09:55:34 +0700

    Update init + header + photo

---
+++ mycourse.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Khóa học của tôi</title>
+        <meta name="viewport" charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <link href = "https://code.jquery.com/ui/1.13.2/themes/black-tie/jquery-ui.css" rel = "stylesheet">
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <?php include('./navbar/header.php'); ?>
+
+        <div class="text-center">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <h1>Khóa học của tôi</h1>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+
+        <?php include('./navbar/footer.php'); ?>
+    </body>
+</html>
