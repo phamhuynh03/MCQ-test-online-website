c94301ee1aa94d7b1638635d1aa9d4b23a63802e
ShawTailer <100937406+ShawTailer@users.noreply.github.com>
2024-04-27 23:41:57 +0700

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -8,23 +8,42 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <link href = "https://code.jquery.com/ui/1.13.2/themes/black-tie/jquery-ui.css" rel = "stylesheet">
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="./style.css">
     </head>
+    <style>
+
+</style>
     <body>
-
-        <div class="text-center">
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <h1>Đăng nhập</h1>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+    <?php 
+        include('./header.php');
+    ?>
+    <div class="container main">
+        <div class="row main2">
+            <div class="col-md-6 right">      
+                <div class="input-box">
+                   <header>Đăng nhập</header>
+                   <form action="login_processing.php"  method="post">
+                   <div class="input-field">
+                        <input type="email" class="input" id="email" name="email" required="">
+                        <label for="email">Email</label> 
+                    </div> 
+                   <div class="input-field">
+                        <input type="password" class="input" id="password" name="password" required="">
+                        <label for="pass">Password</label>
+                    </div> 
+                   <div class="input-field">
+                        <input type="submit" class="submit" value="Đăng nhập">
+                   </div> 
+                    </form>
+                   <div class="signin">
+                    <span>Chưa có tài khoản? <a href="./register.php">Đăng ký</a></span>
+                   </div>
+                </div>  
+            </div>
         </div>
-
+    </div>
+    <?php 
+        include('./footer.php');
+    ?>
     </body>
 </html>
